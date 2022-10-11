a4c5dabdfdb68b8a4df34ab5daeb0deee2e25f73
Damon Bayer <damon@damonbayer.com>
2022-10-11 15:16:25 -0700

    swap to cmdstan backend for overdisp priors model

--- scripts/find_overdisp_priors.R
+++ scripts/find_overdisp_priors.R
@@ -3,38 +3,38 @@
 library(tidyverse)
 library(rstan)
 library(brms)
-options(mc.cores = parallelly::availableCores())
+options(mc.cores = parallelly::availableCores(),
+        brms.backend = "cmdstanr")
 rstan_options(auto_write = TRUE)
 
 source("src/spline_functions.R")
+
 # command args for array job ----------------------------------------------
 args <- commandArgs(trailingOnly=TRUE)
 indic <- as.integer(args[1])
-
 
 county_id_key <- read_csv("data/county_id_key.csv")
 county_id <- county_id_key %>% filter(id == indic) %>% pull(county)
 
 data <- read_csv("data/cases_hospitalizations_by_county.csv") %>%
         filter(county == county_id)
+
 # find hospitalization priors ---------------------------------------------
-
-
 hosp_spline <- run_nb_spline(data = data,
                              response = "hosp")
 
 hosp_overdisp <- choose_kappa_params(hosp_spline)
 
 hosp_params <- hosp_overdisp$par
-# find icu priors ---------------------------------------------------------
 
+# find icu priors ---------------------------------------------------------
 icu_spline <- run_nb_spline(data, response = "icu")
 
 icu_overdisp <- choose_kappa_params(icu_spline)
 
 icu_params <- icu_overdisp$par
-# save the results --------------------------------------------------------
 
+# save the results --------------------------------------------------------
 labels <- c("hosp", "icu")
 
 priors <- rbind(hosp_params, icu_params) %>%
