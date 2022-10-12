2f254aadd1e439868bf92810cedc2338c75d3a84
Damon Bayer <damon@damonbayer.com>
2022-10-12 13:49:12 -0700

    fix overdispersion priors parsing

--- scripts/fit_model_waning.jl
+++ scripts/fit_model_waning.jl
@@ -43,10 +43,10 @@ include(projectdir("src/load_process_data.jl"))
 
 ## Load overdisp priors_only
 overdisp_priors = CSV.read(datadir(string("overdisp_priors_countyid", county_id, ".csv")), DataFrame)
-ϕ_hosp_sd = overdisp_priors[overdisp_priors.labels .== "hosp", :sd] 
-ϕ_hosp_mean = overdisp_priors[overdisp_priors.labels .== "hosp", :mean]
-ϕ_icu_sd = overdisp_priors[overdisp_priors.labels .== "icu", :sd]
-ϕ_icu_mean = overdisp_priors[overdisp_priors.labels .== "icu", :mean]
+ϕ_hosp_sd = overdisp_priors[overdisp_priors.labels .== "hosp", :sd][1]
+ϕ_hosp_mean = overdisp_priors[overdisp_priors.labels .== "hosp", :mean][1]
+ϕ_icu_sd = overdisp_priors[overdisp_priors.labels .== "icu", :sd][1]
+ϕ_icu_mean = overdisp_priors[overdisp_priors.labels .== "icu", :mean][1]
 ## Define Priors
 include(projectdir("src/prior_constants.jl"))
 
