310e66951151bc4b0d9526f25af5452dcb3dbd75
CatalinaMedina <catalmm1@uci.edu>
2023-02-03 15:21:05 -0800

    initial commit for combine_archived_results.R

---
+++ scripts/combine-archived-results.R
@@ -0,0 +1,8 @@
+# specify column names for each format
+
+
+# read in each csv, add date column, and adjust column names to match most recent naming
+
+
+# combine data frames to have most one data frame for all forecasts
+
